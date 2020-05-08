b100774c15135bc43a4a755b3b74a4eca7732814
Sergio Santacruz <sisantacruzm@gmail.com>
2020-05-07 19:29:23 -0500

    1.6

--- Transaccion.py
+++ Transaccion.py
@@ -1,1 +1,1 @@
-transaccion = {'origen': 'wallet', 'operacion': 'registrartransaccion', 'dir1': 'b96dce7c622157c1739505bd0107be4bfb70a28ec27bd8b12b17c86a5f66f614', 'dir2': 'c167b73616df9031a905266bebd3cc669374aa6d2f884905a11bf7389cc308fe', 'dinero': '34'}
+transaccion = {'origen': 'wallet', 'operacion': 'registrartransaccion', 'dir1': '', 'dir2': '', 'dinero': ''}
