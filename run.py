809995d03a72b80353158fc08acafe85ff79eacb
Laura Santacruz <32122480+Laurasantacruz00@users.noreply.github.com>
2020-05-13 19:30:34 -0500

    Update run.py

--- run.py
+++ run.py
@@ -54,8 +54,8 @@ def saldo():#Mostrando saldo al cliente http://142.44.246.23:5596/coordinator
 
 @app.route("/validacion",methods=["GET","POST"])
 def validacion_transaccion():#Validando informacion con el coordinador
-    r = requests.post('http://142.44.246.23:5596/coordinator',datos = jsonify({"wallet":transaccion}))#Pidiendo validacion al coordinador
-    datos = r.get_json() #Respuesta del coordinador
+    r = requests.post('http://142.44.246.23:5596/coordinator',datos={"origen":"wallet","operacion":"registrartransaccion","datos":"dato"})#Pidiendo validacion al coordinador
+    datos = r.json() #Respuesta del coordinador
     respuesta = datos["respuesta"]
     if respuesta.upper()=="TRUE": #Si es true los datos son correctos la transaccion es exitosa
         usuario = {'transaccion':"Existoso"}
